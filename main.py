8763f7d0be3b464acfb9add732b1befea635bb88
Huginn <deadpool>
2018-04-27 10:55:12 +0000

    s

--- main.py
+++ main.py
@@ -2,9 +2,18 @@
 class Hrutar():
 	def __init__():
 		pass
+
+		
 def hrutaSmidur(file): #Byr til 52 object af typunni Hrutar. Notar stillingar fra hrutar.txt
-	with open(file, "w+") as f:
+	with open(file, "r") as f:
 		lines = f.readlines()
 		for n in lines:
 			print("test")
 hrutaSmidur("hrutar.txt")
+
+def main():
+	
+
+
+
+
