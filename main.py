209a21fa670edc2b15098318c770963528e470de
Huginn <deadpool>
2018-04-27 10:41:25 +0000

    Fyrsta push

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 #Höfundir: Huginn Þór Jóhannsson, Jakub
-
+class Hrutar():
+	def __init__():
+		pass
+def hrutaSmidur(file): #Byr til 52 object af typunni Hrutar. Notar stillingar fra hrutar.txt
+	with open(file, "w+") as f:
+		lines = f.readlines()
+		for n in lines:
+			print("test")
+hrutaSmidur("hrutar.txt")
